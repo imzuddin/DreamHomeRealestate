bf69fed1657519c93f23d7a8877b66293da7cc49
imran uddin <imzuddin@gmail.com>
2025-08-06 18:24:53 -0400

    adding sql scripts to the database initialization folder

--- db/init/procedures.sql
+++ db/init/procedures.sql
@@ -1,4 +1,4 @@
-//Staff Hire Proc 
+-- Staff Hire Proc 
 
 CREATE OR REPLACE PROCEDURE staff_hire_sp(
  p_staffno IN VARCHAR2,
@@ -21,3 +21,131 @@ CREATE OR REPLACE PROCEDURE staff_hire_sp(
     );
     COMMIT;
 END;
+/
+
+-- Update Staff Info 
+
+CREATE OR REPLACE PROCEDURE update_staff_contact_or_salary(
+  p_staffno IN VARCHAR2,
+  p_salary IN NUMBER,
+  p_telephone IN VARCHAR2, 
+  p_mobile IN VARCHAR2,
+  p_email IN VARCHAR2
+) IS 
+BEGIN
+  UPDATE DH_STAFF
+  SET
+    salary = NVL(p_salary, salary),
+    telephone = NVL(p_telephone, telephone),
+    mobile = NVL(p_mobile, Mobile),
+    email = NVL(p_email, Email)
+  WHERE StaffNo = p_staffno;
+  COMMIT; 
+END;
+/
+
+-- Get Branch Address 
+
+CREATE OR REPLACE FUNCTION get_branch_address(
+  p_branchno IN VARCHAR2
+) RETURN VARCHAR2 IS 
+  v_address VARCHAR2(200);
+BEGIN 
+  SELECT street || ', ' || city
+  INTO v_address
+  FROM DH_BRANCH
+  WHERE branchno = p_branchno;
+
+  RETURN v_address;
+EXCEPTION
+  WHEN NO_DATA_FOUND THEN
+    RETURN 'Branch Not Found';
+END;
+/
+
+-- Update Branch Info 
+
+CREATE OR REPLACE PROCEDURE update_branch_details(
+  p_branchno IN VARCHAR2,
+  p_street IN VARCHAR2, 
+  p_city IN VARCHAR2, 
+  p_postcode IN VARCHAR2
+) IS
+BEGIN
+  UPDATE DH_BRANCH
+    SET
+      street = NVL(p_street, street),
+      city = NVL(p_city, city),
+      postcode = NVL(p_postcode, postcode)
+    WHERE Branchno = p_branchno;
+  COMMIT;
+END; 
+/
+
+-- New Branch Procedure
+
+CREATE OR REPLACE PROCEDURE new_branch (
+  p_branchno IN VARCHAR2, 
+  p_street IN VARCHAR2, 
+  p_city IN VARCHAR2, 
+  p_postcode IN VARCHAR2
+)
+IS
+BEGIN
+  INSERT INTO DH_BRANCH (Branchno, street, city, postcode)
+  VALUES (p_branchno, p_street, p_city, p_postcode);
+  COMMIT; 
+END;
+/
+
+-- Register New Client 
+
+CREATE OR REPLACE PROCEDURE client_registration_sp(
+  p_clientno IN VARCHAR2,
+  p_fname IN VARCHAR2, 
+  p_lname IN VARCHAR2, 
+  p_telno IN VARCHAR2, 
+  p_street IN VARCHAR2, 
+  p_city IN VARCHAR2, 
+  p_email IN VARCHAR2, 
+  p_preftype IN VARCHAR2,
+  p_maxrent IN NUMBER
+) IS 
+BEGIN
+  INSERT INTO DH_CLIENT (
+    Clientno,
+    fname,
+    lname,
+    telno,
+    street,
+    city,
+    email,
+    preftype,
+    maxrent
+  )
+  VALUES (
+    p_clientno,
+    p_fname,
+    p_lname,
+    p_telno,
+    p_street,
+    p_city,
+    p_email,
+    p_preftype,
+    p_maxrent
+  );
+  COMMIT;
+END;
+/
+
+-- Delete Client 
+
+CREATE OR REPLACE PROCEDURE delete_client_sp(
+  p_clientno IN VARCHAR2
+) 
+IS 
+BEGIN 
+  DELETE FROM DH_CLIENT WHERE Clientno = p_clientno;
+  COMMIT;
+END;
+/
